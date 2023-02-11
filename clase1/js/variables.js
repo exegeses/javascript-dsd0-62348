0b6dc4d57788e911dc6906592fca038b76820d1e
Marcos <exegeses@gmail.com>
2023-02-11 13:07:40 -0300

    variables en javascript

---
+++ clase1/js/variables.js
@@ -0,0 +1,8 @@
+/* para declarar una variable
+   utilizamos la palabra reservada "let" */
+let numero = 62348;
+let frase = 'Hola Mundo';
+
+/* mostramos los varlores de las variables en le docuemto */
+
+document.write(numero);
